567c719f3db26c7b2b6686a1aef806d3274ba561
blue <blue@bluedeMacBook-Pro.local>
2017-11-02 14:40:07 +0800

    Initial Commit

---
+++ LJCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJCache
+//
+//  Created by blue on 2017/11/2.
+//  Copyright © 2017年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJCache
+//
+//  Created by blue on 2017/11/2.
+//  Copyright © 2017年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
